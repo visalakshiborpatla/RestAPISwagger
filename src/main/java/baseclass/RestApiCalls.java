0ecd22289f002b5fc192a0f6c438bae6238182f6
visalakshiborpatla <58298770+visalakshiborpatla@users.noreply.github.com>
2020-02-13 12:01:05 +0000

    Update RestApiCalls.java

--- src/main/java/baseclass/RestApiCalls.java
+++ src/main/java/baseclass/RestApiCalls.java
@@ -8,8 +8,7 @@ import static org.junit.Assert.assertThat;
 public class RestApiCalls {
 
     public static void getPetStatus() {
-        userStub();
-        given().
+                given().
                 when().
                 get("https://petstore.swagger.io/#/pet/").
                 then().
@@ -17,16 +16,5 @@ public class RestApiCalls {
                 statusCode(200).
                 and().body("name", is("Doggie")).
                 and().body("status", is("available"));
-    }
-
-    public static void userStub() {
-        stubFor(get(urlPathMatching("/petStore/.*"))
-                .withRequestBody(matching("<input>NAME</input>"))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(body)));
-        assertThat(testClient.get("").statusCode(), is(200));
-        assertThat(testClient.get("").statusCode(), is(404));
     }
 }
